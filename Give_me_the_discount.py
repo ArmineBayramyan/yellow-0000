91ddb6ac7ac2a815ba9d63653609c30eca5b2a6b
ArmineBayramyan <79363438+ArmineBayramyan@users.noreply.github.com>
2022-03-28 15:47:34 +0400

    Create  Give_me_the_discount.py

---
+++ Give_me_the_discount.py
@@ -0,0 +1,2 @@
+'''Create a function in Python that accepts two parameters. The first should be the full price of an item as an integer.
+The second should be the discount percentage as an integer.'''
